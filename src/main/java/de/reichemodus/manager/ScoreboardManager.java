84b59820f2239d80e390e038867d30b6b0ae1f29
TommyCodet <tommycodet247@gmail.com>
2026-06-14 14:16:42 +0200

    Create ScoreboardManager.java

---
+++ src/main/java/de/reichemodus/manager/ScoreboardManager.java
@@ -0,0 +1,12 @@
+package de.reichemodus.manager;
+
+import de.reichemodus.ReicheModus;
+
+public final class ScoreboardManager {
+
+    private final ReicheModus plugin;
+
+    public ScoreboardManager(ReicheModus plugin) {
+        this.plugin = plugin;
+    }
+}
