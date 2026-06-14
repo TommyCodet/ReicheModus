d50828721d5535dd25f793b415ca3afaeb810d86
TommyCodet <tommycodet247@gmail.com>
2026-06-14 15:40:05 +0200

    Update DeathListener.java

--- src/main/java/de/reichemodus/listener/DeathListener.java
+++ src/main/java/de/reichemodus/listener/DeathListener.java
@@ -8,7 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.Bukkit;
 
 public final class DeathListener implements Listener {
 
